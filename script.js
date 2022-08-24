480421a405aa6171132f842c1e28d50113d9f3bb
macas5 <mkalinauskas1993@gmail.com>
2022-08-24 13:20:41 +0300

    Add getFirstNamesFiltered function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const fetchData = async (url) => {
+const fetchData = (url) => {
     return fetch(url).then((response) => response.json());
 }
 
@@ -13,6 +13,21 @@ const getUsernames = async (url) => {
     return usernameList;
 }
 
-
+const getFirstNamesFiltered = async (url, hairColor, age) => {
+    const response = await fetchData(url);
+    const firstNameList =[];
+    for (let i = 0; i < response.users.length; i++) {
+        if (response.users[i].hasOwnProperty('firstName') && response.users[i].hasOwnProperty('hair')){
+            if (response.users[i].hasOwnProperty('age') && response.users[i].hair.hasOwnProperty('color')){
+                if ((response.users[i].age > age) && (response.users[i].hair.color == hairColor)){
+                    firstNameList.push(response.users[i].firstName);
+                }
+            }
+        }
+    }
+    return firstNameList;
+}
 
 getUsernames('https://dummyjson.com/users').then((data) => console.log(data));
+
+getFirstNamesFiltered('https://dummyjson.com/users', 'Black', 25).then((data) => console.log(data));
